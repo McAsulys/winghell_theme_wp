5f910c52e898190bf121dda1fe6efb4e04a000d8
McAsulys <benedict.dourlens@gmail.com>
2017-09-26 10:51:29 +0200

    Le css est charger Way !

--- functions.php
+++ functions.php
@@ -6,6 +6,4 @@ function load_style_scripts(){
   //wp_enqueue_style('load_style_boostrap-core', get_template_directory_uri().'/css/bootstrap.min.css', array(), "1.0.0", 'all');
 } //exit load_style_scripts
 
-add_action('wp_enqueue_script', 'load_style_scripts');
-
- ?>
+add_action('wp_enqueue_scripts', 'load_style_scripts');
